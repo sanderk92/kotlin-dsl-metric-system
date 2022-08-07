d8a0a724a3aad174d1d2af575f7ac7809efd9aa5
Sander <sanderkrabbenborg@hotmail.com>
2022-08-07 22:50:26 +0200

    Improve DynamicMeasure

--- src/main/kotlin/com/example/measure/DynamicMeasure.kt
+++ src/main/kotlin/com/example/measure/DynamicMeasure.kt
@@ -1,20 +1,27 @@
 package com.example.measure
 
-data class DynamicMeasure<T, U>(val numerator: Measure<T>, val denominator: Measure<U>) {
+class DynamicMeasure<T, U> private constructor(val numerator: Measure<T>, val denominator: Measure<U>) {
     override fun toString() = "$numerator / $denominator"
+
+    companion object {
+        fun <T, U> create(numerator: Measure<T>, denominator: Measure<U>) = DynamicMeasure(
+            numerator = numerator,
+            denominator = denominator,
+        )
+    }
 }
 
-infix fun <T, A> Measure<T>.per(other: Measure<A>) = DynamicMeasure(
+infix fun <T, A> Measure<T>.per(other: Measure<A>) = DynamicMeasure.create(
     numerator = this,
     denominator = other,
 )
 
-operator fun <T, U> DynamicMeasure<T, U>.div(divisor: Number) = DynamicMeasure(
+operator fun <T, U> DynamicMeasure<T, U>.div(divisor: Number) = DynamicMeasure.create(
     numerator = numerator / divisor,
     denominator = denominator,
 )
 
-operator fun <T, U> DynamicMeasure<T, U>.times(multiplier: Number) = DynamicMeasure(
+operator fun <T, U> DynamicMeasure<T, U>.times(multiplier: Number) = DynamicMeasure.create(
     numerator = numerator * multiplier,
     denominator = denominator,
 )
