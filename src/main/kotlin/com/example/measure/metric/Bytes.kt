034a3a7dac75b1c45b3997165e91f23455fbbf97
Sander <sanderkrabbenborg@hotmail.com>
2022-08-10 21:01:14 +0200

    Add support for Bytes

---
+++ src/main/kotlin/com/example/measure/metric/Bytes.kt
@@ -0,0 +1,25 @@
+package com.example.measure.metric
+
+import java.math.BigDecimal.ONE
+
+class Byte(multiplier: MetricMultiplier) : Metric<Byte>(multiplier, "b") {
+    init {
+        check(multiplier.factor >= ONE) { "Byte does not support multiplier '${multiplier}'" }
+    }
+
+    override fun normalize() = Byte(Base)
+}
+
+val byte: Metric<Byte> = Byte(Base)
+
+val Yotta.byte: Metric<Byte> get() = Byte(this)
+val Zetta.byte: Metric<Byte> get() = Byte(this)
+val Exa.byte: Metric<Byte> get() = Byte(this)
+val Peta.byte: Metric<Byte> get() = Byte(this)
+val Tera.byte: Metric<Byte> get() = Byte(this)
+val Giga.byte: Metric<Byte> get() = Byte(this)
+val Mega.byte: Metric<Byte> get() = Byte(this)
+val Kilo.byte: Metric<Byte> get() = Byte(this)
+val Hecto.byte: Metric<Byte> get() = Byte(this)
+val Deca.byte: Metric<Byte> get() = Byte(this)
+val Base.byte: Metric<Byte> get() = Byte(this)
