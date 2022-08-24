09f837f0ac955252da4e6de9d5341799b0b4eb1e
Sander <sanderkrabbenborg@hotmail.com>
2022-08-24 20:36:21 +0200

    Allow both Number and BigDecimal to be used for creating measures

--- src/test/kotlin/com/example/measure/MeasureTest.kt
+++ src/test/kotlin/com/example/measure/MeasureTest.kt
@@ -1,12 +1,17 @@
 package com.example.measure
 
 import com.example.measure.invoke
-import com.example.measure.metric.Kilo
-import com.example.measure.metric.Milli
-import com.example.measure.metric.meter
+import com.example.measure.metric.*
 import com.example.measure.normalized
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 class MeasureTest {
+
+    fun test() {
+        val it = BigDecimal.TEN(liter)
+
+    }
+
 }
